80d0abc5211e83fd8c0f76bc5db87e5a6ac53b17
James Lambert <lambertjamesd@gmail.com>
2020-12-22 14:07:02 -0700

    Put menu back in

--- src/level/level.c
+++ src/level/level.c
@@ -170,7 +170,6 @@ void levelHudRender(void* data, struct GraphicsState* state, struct FontRenderer
 
     int i;
     for (i = 0; i < gScene.activeViewportCount && i < 2; ++i) {
-        break;
         if (gInputMask[i] & InputMaskFreeCamera) {
             fontRendererSetScale(fontRenderer, 1.0f, gScreenYScale);
 
